8fe32aac1a934c675b63abeffcf7355f9a085633
DESKTOP-3ORTB40\ust <ustnano@naver.com>
2017-08-21 16:32:00 +0900

    첫번째 버전 입니다.

---
+++ src/lab1/Test1.java
@@ -0,0 +1,10 @@
+package lab1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/lab1/Test2.java
@@ -0,0 +1,10 @@
+package lab1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test2 class");
+
+	}
+
+}
