938bb3d60aa98d8c0eab77a2219e779899066de8
DESKTOP-3ORTB40\ust <ustnano@naver.com>
2017-08-21 16:46:16 +0900

    두번째 버전 입니다.

--- src/lab1/Test2.java
+++ src/lab1/Test2.java
@@ -3,7 +3,7 @@ package lab1;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("Test2 class");
+		System.out.println("Test2.....update class");
 
 	}
 
--- src/lab2/Test1.java
+++ src/lab2/Test1.java
@@ -3,7 +3,7 @@ package lab2;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Test1....last class");
 
 	}
 
